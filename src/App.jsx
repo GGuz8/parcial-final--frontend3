ccb585766d7ffce48cd3693a6e1155a0de9c78c1
GGuz8 <guzma.gf@gmail.com>
2023-09-14 18:11:22 -0300

    formulario y validaciones completadas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Form from './Components/Form'
 import Layout from './Routes/Layout'
-import Body from './Components/Body'
 import Home from './Routes/Home'
 import Detail from './Routes/Detail'
 import Favs from './Routes/Favs'
@@ -44,6 +43,7 @@ function App() {
                 <Route path='/contact' element={<Contact/>}/>
                 <Route path='/detail/:id' element={<Detail/>}/>  
               </Routes>
+              <Footer/>
             </Layout>
 
           </GlobalContextProvider>
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,70 @@
 import React from "react";
+import { useState } from "react";
 
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [formValid, setFormValid] = useState(false);
+  const [submited, setSubmited] = useState(false)
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const regExEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+  const regExUser = /[a-zA-Z][a-zA-Z0-9-_]{3,32}/gi;
+
+  const emailValidado = regExEmail.test(email);
+  const userValidado = regExUser.test(username);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setSubmited(true);
+    setFormValid(emailValidado && userValidado);
+  };
+
+  const renderFormStatus = () => {
+    if (formValid) {
+      return (
+        <div className="formOk">
+          <p>Formulario enviado</p>
+        </div>
+      );
+    } else if(submited) {
+      return (
+        <div className="formError">
+          <p>Email incorrecto</p>
+        </div>
+      );
+    }
+    return null
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="usernameInput">Username</label>
+        <input
+          id="usernameInput"
+          type="text"
+          value={username}
+          onChange={onChangeUsername}
+        />
+        <label htmlFor="emailInput">Email</label>
+        <input
+          id="emailInput"
+          type="text"
+          value={email}
+          onChange={onChangeEmail}
+        />
+        <button type="submit">Enviar</button>
       </form>
+      {renderFormStatus()}
     </div>
   );
 };
